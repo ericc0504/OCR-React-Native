a43ad3e2aa633208a1c634ac34550a1a65f36c12
Eric <eric.c0504@gmail.com>
2020-08-30 23:02:11 +0800

    Added ocr result screen

--- app/screens/HistoryScreen.js
+++ app/screens/HistoryScreen.js
@@ -1,12 +1,126 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  FlatList,
+  RefreshControl,
+} from "react-native";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import dataFormat from "dateformat";
 
-export default function HistoryScreen({ navigation }) {
+import ServerOperation from "../ServerOperation";
+
+export default function HistoryScreen({ route, navigation }) {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [ocrResults, setOcrResults] = useState([]);
+  useEffect(() => {
+    getData();
+    return () => {
+      setOcrResults([]);
+    };
+  }, []);
+
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableHighlight
+        activeOpacity={0.5}
+        underlayColor="silver"
+        onPress={async () => {
+          navigation.push("OCRResult", item);
+        }}
+      >
+        <View style={styles.rowViewContainer}>
+          <View
+            style={{
+              borderColor: "darkgray",
+              borderWidth: 1,
+              borderRadius: 10,
+            }}
+          >
+            <Image
+              style={{
+                height: 64,
+                width: 64,
+                borderRadius: 10,
+              }}
+              source={{
+                uri: item.base64,
+              }}
+            />
+          </View>
+
+          <Text style={{ paddingLeft: 15, alignSelf: "center", fontSize: 16 }}>
+            {dataFormat(new Date(item.createdAt), "yyyy-mm-dd HH:MM:ss")}
+          </Text>
+        </View>
+      </TouchableHighlight>
+    );
+  };
+
+  function getListViewItemSeparator() {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "90%",
+          backgroundColor: "#808080",
+        }}
+      />
+    );
+  }
+
+  function onRefresh() {
+    setIsRefreshing(true);
+    setTimeout(() => {
+      getData();
+      setIsRefreshing(false);
+    }, 1000);
+  }
+
+  async function getData() {
+    try {
+      let res = await ServerOperation.getPreviousOcrResult();
+      if (res) {
+        res.map((x) => {
+          x.id = x._id;
+          x.base64 =
+            "data:image/jpg;base64," +
+            String.fromCharCode.apply(null, x.thumbnail.data);
+        });
+        setOcrResults(res);
+      }
+    } catch (err) {
+      console.log(err);
+      alert("Error occuured when getting previous ocr result");
+    }
+  }
+
   return (
-    <View>
-      <Text>History Screen</Text>
+    <View styles={styles.mainContainer}>
+      <FlatList
+        style={{ height: "100%" }}
+        data={ocrResults}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={getListViewItemSeparator}
+        refreshControl={
+          <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
+        }
+        enableEmptySections={true}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+  rowViewContainer: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    flexDirection: "row",
+  },
+});
---
+++ app/screens/OCRResultScreen.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Clipboard,
+  Image,
+  Dimensions,
+  ScrollView,
+} from "react-native";
+import ServerOperation from "../ServerOperation";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const deviceHeight = Dimensions.get("window").height;
+const deviceWidth = Dimensions.get("window").width;
+const imgHeight = deviceHeight * 0.5;
+const contentWidth = deviceWidth * 0.9;
+
+export default function OCRResultScreen({ route, navigation }) {
+  const [base64, setBase64] = useState(route.params.base64);
+  useEffect(() => {
+    getFullSizeImg();
+    return () => {};
+  }, []);
+
+  const getFullSizeImg = async () => {
+    try {
+      let res = await ServerOperation.getFullSizeImg(route.params.id);
+      var base64 = "data:image/jpg;base64,";
+      var chunk = 8 * 1024;
+      let i;
+      for (i = 0; i < res.img.data.length / chunk; i++) {
+        base64 += String.fromCharCode.apply(
+          null,
+          res.img.data.slice(i * chunk, (i + 1) * chunk)
+        );
+      }
+      base64 += String.fromCharCode.apply(null, res.img.data.slice(i * chunk));
+      setBase64(base64);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <ScrollView style={styles.mainContainer}>
+      <Image source={{ uri: base64 }} resizeMode="contain" style={styles.img} />
+      <TouchableOpacity
+        onPress={() => {
+          Clipboard.setString(route.params.text);
+          alert("Copied");
+        }}
+      >
+        <Text style={styles.helpMsg}>Tap to copy result</Text>
+        <Text style={styles.ocrResult}>{route.params.text}</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+  img: {
+    marginTop: 10,
+    height: imgHeight,
+    width: contentWidth,
+    alignSelf: "center",
+    borderWidth: 1,
+    borderColor: "silver",
+    borderRadius: 10,
+    backgroundColor: "lightgray",
+  },
+  ocrResult: {
+    flex: 1,
+
+    width: contentWidth,
+    alignSelf: "center",
+    backgroundColor: "white",
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 5,
+    overflow: "hidden",
+  },
+  helpMsg: {
+    marginTop: 15,
+    marginBottom: 2,
+    alignSelf: "center",
+    width: contentWidth,
+    color: "#808080",
+  },
+});
